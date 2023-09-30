c020170ec0e6b6ae11f98e598970f21e190760c5
Ragnarloki <145421521+Ragnarloki@users.noreply.github.com>
2023-09-30 09:23:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+var numberofdrumbuttons = document.querySelectorAll(".drum").length;
+for (var i=0; i<numberofdrumbuttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+     var buttonInnerHTML=this.innerHTML;
+        switch (buttonInnerHTML){
+          case "w":
+            var tom1=new Audio('tom-1.mp3');
+            tom1.play();
+          
+            
+          break;
+
+          case "a":
+            var tom2=new Audio('tom-2.mp3');
+            tom2.play();
+          break;
+
+          case "s":
+            var tom3=new Audio('tom-3.mp3');
+            tom3.play();
+          break;
+
+          case "d":
+            var tom4=new Audio('tom-4.mp3');
+            tom4.play();
+          break;
+
+          case "j":
+            var crash=new Audio('crash.mp3');
+            crash.play();
+          break;
+
+          case "k":
+            var kickbass=new Audio('kick-bass.mp3');
+            kickbass.play();
+          break;
+
+          case "l":
+            var snare=new Audio('snare.mp3');
+            snare.play();
+          break;
+          default:  
+
+        }
+    }) ;
+}
